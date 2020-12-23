fccd28c66370f91260a454a5afe520b8e0a7cd67
oscarbaltasar <oscar@Ordenador.localdomain>
2020-12-23 19:10:28 +0100

    Bucle de lectura e identificación de comandos

--- simul_ext_esqueleto.c
+++ simul_ext_esqueleto.c
@@ -7,7 +7,7 @@
 #define LONGITUD_COMANDO 100
 
 void Printbytemaps(EXT_BYTE_MAPS *ext_bytemaps);
-int ComprobarComando(char *strcomando, char *orden, char *argumento1, char *argumento2);
+int ComprobarComando(char *strcomando);
 void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup);
 int BuscaFich(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, 
               char *nombre);
@@ -29,17 +29,22 @@ void GrabarDatos(EXT_DATOS *memdatos, FILE *fich);
 
 int main()
 {
-	 char *comando[LONGITUD_COMANDO];
-	 char *orden[LONGITUD_COMANDO];
-	 char *argumento1[LONGITUD_COMANDO];
-	 char *argumento2[LONGITUD_COMANDO];
-	 
+	 char comando[LONGITUD_COMANDO];
+         int f_comando;
 	 int i,j;
 	 unsigned long int m;
      EXT_SIMPLE_SUPERBLOCK ext_superblock;
+      //Variables que necesitamos cambiar
+       //s_free_blocks_count;
+       //s_free_inodes_count;
      EXT_BYTE_MAPS ext_bytemaps;
+      //Todas son cambiadas (-relleno):
+       //bmap_inodos;
+       //bmap_bloques;
      EXT_BLQ_INODOS ext_blq_inodos;
+      //Cambiar todo menos relleno
      EXT_ENTRADA_DIR directorio[MAX_FICHEROS];
+      //Cambiar solo dir_nfich[] en caso de que no tengamos que crear directorios;
      EXT_DATOS memdatos[MAX_BLOQUES_DATOS];
      EXT_DATOS datosfich[MAX_BLOQUES_PARTICION];
      int entradadir;
@@ -54,13 +59,40 @@ int main()
      }
      fread(&datosfich, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, fent);    
      
-     
+     //Inicializacion variables
      memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&datosfich[0], SIZE_BLOQUE);
      memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], SIZE_BLOQUE);
      memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&datosfich[1], SIZE_BLOQUE);
      memcpy(&ext_blq_inodos,(EXT_BLQ_INODOS *)&datosfich[2], SIZE_BLOQUE);
      memcpy(&memdatos,(EXT_DATOS *)&datosfich[4],MAX_BLOQUES_DATOS*SIZE_BLOQUE);
-     
+
+
+     do{
+	printf("\n>> ");
+	fgets(comando, LONGITUD_COMANDO, stdin);
+	f_comando = ComprobarComando(comando);
+	switch(f_comando){
+		case 0: //Salir
+		break;
+		case 1: //Info
+		break;
+		case 2: //bytemaps
+		break;
+		case 3: //dir
+		break;
+		case 4: //rename
+		break;
+		case 5: //imprimir
+		break;
+		case 6: //remove
+		break;
+		case 7: //copy
+		break;
+		case 8: //ERROR
+			printf("ERROR: Comando ilegal [bytemaps,copy,dir,info,imprimir,rename,remove,salir]");
+		break;
+	}
+    }while(f_comando!=0);
      // Buce de tratamiento de comandos
      //for (;;){
 //		 do {
@@ -72,7 +104,7 @@ int main()
 //            Directorio(&directorio,&ext_blq_inodos);
 //            continue;
 //            }
-         // Escritura de metadatos en comandos rename, remove, copy     
+         // Escritura de metadatos en comandos rename, remove, copy
 //         Grabarinodosydirectorio(&directorio,&ext_blq_inodos,fent);
 //         GrabarByteMaps(&ext_bytemaps,fent);
 //         GrabarSuperBloque(&ext_superblock,fent);
@@ -87,4 +119,28 @@ int main()
 //            return 0;
 //         }
 //     }
+}
+
+int ComprobarComando(char *strcomando){
+	char string[LONGITUD_COMANDO];
+	sscanf(strcomando, "%s", string);
+	if(strcmp(string, "info") == 0){
+		return 1;
+	} else if(strcmp(string, "bytemaps") == 0){
+                return 2;
+        } else if(strcmp(string, "dir") == 0){
+                return 3;
+        } else if(strcmp(string, "rename") == 0){
+                return 4;
+        } else if(strcmp(string, "imprimir") == 0){
+                return 5;
+        } else if(strcmp(string, "remove") == 0){
+                return 6;
+        } else if(strcmp(string, "copy") == 0){
+                return 7;
+        } else if(strcmp(string, "salir") == 0){
+                return 0;
+        } else {
+                return 8;
+        }
 }
